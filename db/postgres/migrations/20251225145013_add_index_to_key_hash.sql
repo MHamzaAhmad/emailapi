6684130c96450a91af3bed8ba593fc13729f765e
MHamzaAhmad <hmzaawan88@gmail.com>
2025-12-26 02:53:09 +0500

    chore: Update migration file headers to use goose directives.

--- db/postgres/migrations/20251225145013_add_index_to_key_hash.sql
+++ db/postgres/migrations/20251225145013_add_index_to_key_hash.sql
@@ -1,8 +1,7 @@
--- Up Migration
-
+-- +goose Up
 -- Optimize index for key_hash lookups
 DROP INDEX IF EXISTS idx_api_keys_key_hash;
 CREATE INDEX idx_api_keys_key_hash ON api_keys(key_hash);
 
--- Down Migration
+-- +goose Down
 DROP INDEX IF EXISTS idx_api_keys_key_hash;
